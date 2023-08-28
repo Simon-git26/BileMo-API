5f1c1d1de1575e2b2d999f2976006cb54beb08e5
Simon-git26 <simoncestmoi@hotmail.fr>
2023-08-28 12:56:32 +0200

    Ajout dun user, en lui associant un client existant en requete

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,13 +8,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+
+
+// Utiliser pour la suppression
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+// Url Generator en cas de POST Phone
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 
 // Importer l'entite pour le param converter
 use App\Entity\User;
 use App\Entity\Client;
 // Serializer
 use Symfony\Component\Serializer\SerializerInterface;
+//use Symfony\Component\Serializer\SerializationContext;
+//use JMS\Serializer\SerializationContext;
+
+
 
 class UserController extends AbstractController
 {
@@ -54,7 +65,7 @@ class UserController extends AbstractController
 
 
 
-     /**
+    /**
      * @Route("/api/clients/{id}/users", name="app_client_users", methods={"GET"})
      * 
      * ************************************** Retourne la liste des users selon l'id du client donnée *******************************************
@@ -67,4 +78,58 @@ class UserController extends AbstractController
         return new JsonResponse($UsersListByClient, Response::HTTP_OK, [], true);
 
     }
+
+
+
+
+
+
+    /**
+     * @Route("/api/user", name="app_createUser", methods={"POST"})
+     * 
+     * ************************************** Ajouter un utilisateur liés a un client *******************************************
+     * 
+    */
+    public function createUser(ClientRepository $clientRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        // Récupéré les données posté, et les déserializer dans un objet User
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+
+
+        // Récupération de l'ensemble des données envoyées sous forme de tableau
+        $content = $request->toArray();
+
+        // Récupération de l'idAuthor. S'il n'est pas défini, alors on met -1 par défaut.
+        $idClient = $content['id'] ?? -1;
+
+        // On cherche l'auteur qui correspond et on l'assigne au livre.
+        // Si "find" ne trouve pas l'auteur, alors null sera retourné.
+        
+        $user->setClient($clientRepository->find($idClient));
+
+        $em->persist($user);
+        $em->flush();
+
+
+
+
+
+
+
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+      
+
+        /* Calculer l'url pour pouvoir indiquez l'url du User que l"on vient de créer en header pour pouvoir aller tester rapidement 
+        *  Si la création c'est bien passé en récupérant mon nouveau user selon son id par exemple
+        */
+        $location = $urlGenerator->generate('app_detailUser', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        // Code 201 pour la création
+        // Retourner dans le headers de la reponse que j'ai besoin de mon nouvel element location
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["Location" => $location], true);
+
+    }
+
+    
 }
